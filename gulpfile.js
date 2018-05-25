40062ad3af1943ee9ab9121f7b7f5f7956cf52f0
Er.Ellison <er.ellison.nyc@gmail.com>
2018-05-26 03:01:35 +0430

    base Structure

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+
+// Import Some requirments
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const imageMin = require('gulp-imagemin')
+const rename = require('gulp-rename')
+const concat = require('gulp-concat')
+const browserSync = require('browser-sync').create()
+
+
+
