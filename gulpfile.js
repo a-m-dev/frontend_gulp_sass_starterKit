104d2250997ef80944f205995a4e63918d5149ba
Er.Ellison <er.ellison.nyc@gmail.com>
2018-05-26 03:02:03 +0430

    Base Structure

--- gulpfile.js
+++ gulpfile.js
@@ -9,4 +9,73 @@ const concat = require('gulp-concat')
 const browserSync = require('browser-sync').create()
 
 
+// Build Folder
+const BUILD_FOLDER = 'public/'
+
+
+// Coping Html Files
+gulp.task('markup' , () => {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest(BUILD_FOLDER))
+})
+
+
+
+
+// Compile Sass
+gulp.task('sass' , () => {
+    gulp.src('src/sass/app.scss')
+        .pipe(sass().on('error' , sass.logError))
+        .pipe(gulp.dest(BUILD_FOLDER + 'css'))
+        .pipe(browserSync.stream())
+})
+
+
+
+
+// Concat Js files
+gulp.task('scripts' , () => {
+    gulp.src('src/js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(BUILD_FOLDER + 'js'))
+        .pipe(browserSync.stream())
+})
+
+
+
+
+// Minify Images
+gulp.task('minify' , () => {
+    gulp.src('src/img/*')
+        .pipe(imageMin())
+        .pipe(gulp.dest(BUILD_FOLDER + 'img'))
+        .pipe(browserSync.stream())
+})
+
+
+
+
+// Serve
+gulp.task('serve' , ['markup' , 'sass' , 'scripts' , 'minify'] , () => {
+
+    browserSync.init({
+        server: BUILD_FOLDER
+    })
+
+
+    gulp.watch('src/*.html' , ['markup'])
+    gulp.watch('src/js/*.js' , ['scripts'])
+    gulp.watch('src/sass/app.scss' , ['sass'])
+    gulp.watch('src/*.html').on('change' , browserSync.reload)
+
+})
+
+
+
+
+gulp.task('default' , ['serve'])
+
+
+
 
---
+++ readme.md
@@ -0,0 +1,34 @@
+-- src
+   |-- img
+   |   |-- img.jpg
+   |-- js
+   |   |-- app.js
+   |-- sass
+   |   |-- 0_dependencies
+   |   |   |-- _frameworks, Like bootstrap & boutbon
+   |   |   |-- _variables
+   |   |   |-- _mixins
+   |   |   |-- _function
+   |   |   |-- _hub
+   |   |-- 1_base
+   |   |   |-- _reset
+   |   |   |-- _elementStyles
+   |   |   |-- _hub
+   |   |-- 2_layouts
+   |   |   |-- _sections&Layouts
+   |   |   |-- _hub
+   |   |-- 3_modules
+   |   |   |-- _everyModuleCLass
+   |   |   |-- _hub
+   |   |-- 4_states
+   |   |   |-- _states
+   |   |   |-- _mediaQueries
+   |   |   |-- _hub
+   |   |   app.scss
+   |-- index.html
+   |-- ...html
+-- .gitignore
+-- gulpfile.js
+-- package-lock.json
+-- package.json
+-- readme.md
